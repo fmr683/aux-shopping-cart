646e92c87fdd6510cf96110be3b0ce936a993f3f
Fazlul <faz@keeneye.solutions>
2019-04-02 16:46:50 +0530

    completed the product module

---
+++ tests/integration/product.test.js
@@ -0,0 +1,65 @@
+const request = require('supertest');
+const Product = require('../../models/product');
+const User = require('../../models/user');
+const config = require('config');
+
+var server;
+
+describe('Check User Authentication ', () => {
+    beforeEach(() => { server = require('../../bin/www'); })
+    afterEach(async () => { server.close(); });
+
+    let tokenId; 
+
+    var header = {
+        'api-key': config.get('api.key'),
+        'api-secret': config.get('api.secret'),
+        'Content-type': 'application/json'
+    }
+  
+    const noProductExec = () => {
+      return request(server).get('/v1/products')
+      .set({Authorization: tokenId.accessToken })
+      .set(header);
+    }
+  
+    const product =  new Product();
+
+    beforeEach(async () => {
+
+        let user =  new User();
+        tokenId = await user.generateAuthenticationToken();
+    });
+  
+    it('should return 404 - no products', async () => {
+
+       
+        await product.delete();
+
+        let res = await noProductExec();
+        expect(res.status).toBe(404);
+    });
+
+
+    const productExec = () => {
+        return request(server).get('/v1/products')
+        .set({Authorization: tokenId.accessToken })
+        .set(header);
+      }
+
+    it('should return 200 - product available', async () => {
+
+        let data = []
+        data["p_title"] = "Product 1";
+        data["p_desc"] = "Product 1 description";
+        data["p_price"] = 100;
+        data["p_qty"] = 1;
+
+        await product.insert(data);
+
+        let res = await productExec();
+        expect(res.status).toBe(200);
+    });
+
+  
+  });
